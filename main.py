291ffae02a55156f0b77389ac5d35424a79a7dc2
Cerelia <125533898+cerelia969@users.noreply.github.com>
2023-03-20 04:44:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+qn1 = input('Who was the first principal? ')
+
+if qn1 == 'Mr Manogaran Suppiah':
+  print('Correct')
+
+else :
+  print('Wrong')
+
+qn2 = input('Who is our current principal?(Put full name) ')
+
+if qn2 == 'Mr Heng Yew Seng':
+  print('Correct')
+
+else :
+  print('Wrong')
+  
+  
+  qn3 = input('What is our school motto? ')
+
+if qn3 == 'Discere Servire – Non Mihi Solum':
+  print('Correct')
+
+else :
+  print('Wrong')
+  qn4 = input('How many houses do we have ')
+
+if qn4 == '4':
+  print('Correct')
+
+else :
+  print('Wrong')
+  qn5 = input('What is the purple house? ')
+
+if qn5 == 'Athena':
+  print('Correct')
+
+else :
+  print('Wrong')
